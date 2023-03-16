e15bdce9a20ef1e22b447654cd93705c2ec34f45
itzcoatl397 <itzcoatl3b@gmail.com>
2023-03-16 09:34:03 -0600

    Se modifico el carrito

--- src/components/Ordenar.vue
+++ src/components/Ordenar.vue
@@ -9,6 +9,12 @@
         <h2 class="flex justify-around font-bold text-2xl">Iva <span>16%</span></h2>
         <h2 class="flex justify-around font-bold text-2xl">Total  <span>0</span></h2>
 
+        <button class="bg-orange-600 
+        w-full lg:w-1/2
+        rounded-md h-12
+       
+        lg:mx-28
+        mt-3 text-white text-2xl">Ordenar</button>
     </div>
 </div>
 </template>
